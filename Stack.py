978f980a8907a492398d75f5e7bb3a1faf666927
bjanani <vjanani@gmail.com>
2014-02-01 12:29:12 +0530

    Create Stack.py
    STACK is used for push and pop operation in which stack is a Last-in-First-out Data structure.

---
+++ Stack.py
@@ -0,0 +1,36 @@
+#A program to implent list using Stack#
+#!/user/bin/python
+class myStack:
+     def __init__(self):
+         self.container = []
+     def isEmpty(self):
+         return self.size() == 0
+     def push(self, item):
+         self.container.append(item)
+     def pop(self):
+         return self.container.pop(0)
+     def size(self):
+         return len(self.container)
+s = myStack()
+s.push('1')
+s.push('2')
+print(s.pop())
+print()
+
+#A program to implement Dict using Stack#
+import dict
+ countries.push=dict(USA={'capital':'washington','currency':'usd'},{Japan={'capital':'tokyo','currency':'tpy'},{India={'capital':'newdelhi','currency':'rupee'})
+ country=input("enter USA or Japan or India")
+ countries.pop=dict(USA)
+  print(countries[country]["capital"])
+ print()
+ 
+#A program to implement Tuple in stack#
+tup 1=('software engineering','datastructure',1997,2000);
+tup 2=(1,2,3,4,6,7);
+ print push.tup 1[0],tup 1[0]
+ print push.tup2[1:4],tup2[1:4]
+tup3=('a','b','c')
+tup4={'d','e','f')
+print tup():
+
